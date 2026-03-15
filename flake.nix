238111f98ab6c87497ba503c9a6b5bb5970587fa
Po Co <polniolino@gmail.com>
2026-03-15 10:38:38 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "My own wayland compositor in rust";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              cargo
+              rustc
+            ];
+          };
+        }
+      );
+    };
+}
